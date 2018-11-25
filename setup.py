26c66ceb0f019404164fde346be7ba4e527f121f
Phill <phill@peelvalley.com.au>
2018-11-25 17:17:02 +1100

    Add initial files

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='dockerlib',  
+    version='0.1',
+    #scripts=['dockerlib'] ,
+    author="Peel Valley Softwware",
+    author_email="info@peelvalley.com.au",
+    description="Custom docker client wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    #url="https://github.com/phillmac/DockerLib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+    ],
+)
