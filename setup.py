3866ee378f733de63799393b65ed342a71575f8d
Phill <phill@peelvalley.com.au>
2018-11-26 11:56:18 +1100

    Add requirement for docker client lib

--- setup.py
+++ setup.py
@@ -12,6 +12,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     #url="https://github.com/phillmac/DockerLib",
     packages=setuptools.find_packages(),
+    install_requires=[
+   'docker',
+    ],
     classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
