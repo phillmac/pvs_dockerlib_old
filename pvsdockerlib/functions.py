214d0f9ae002b43ad8ee249be7953e0df8ec4d47
Phill <phill@peelvalley.com.au>
2019-01-09 17:41:48 +1100

    Cleanup, use logging instead of print

--- pvsdockerlib/functions.py
+++ pvsdockerlib/functions.py
@@ -1,7 +1,6 @@
 from docker.errors import APIError, NotFound
 from requests import ConnectionError
-import docker, threading, os
-
+import docker, threading, os, logging
 
 
 def create_client(use_local_socket=False):
@@ -21,16 +20,17 @@ def docker_run_settings(docker_client, docker_settings):
 
 
 def docker_start_settings(docker_client, docker_settings):
+    logger = logging.getLogger(__name__)
     container_name = docker_settings['name']
-    print('Starting container: ', container_name)
+    logger.info('Starting container: {container_name}'.format(**locals()))
     if len(docker_client.containers.list(all=True, sparse=True, filters={'name':container_name})) > 0:
-        print('Found container')
+        logger.info('Found container')
         container = docker_client.containers.get(container_name)
         container.start()
-        print('Started')
+        logger.info('Started')
         return True
     else:
-        print('Container not found, creating')
+        logger.info('Container not found, creating')
         docker_run_settings(docker_client, docker_settings)
         return True
 
@@ -51,32 +51,29 @@ def docker_restart_settings(docker_client, docker_settings):
     if len(docker_client.containers.list(all=True, sparse=True, filters= {'name':container_name})) > 0:
         container = docker_client.containers.get(container_name)
         container.restart(timeout=60)
-        """ container.wait(
-            timeout = 30,
-            condition = 'not-running'
-        ) """
         return True
     else:
         docker_run_settings(docker_client, docker_settings)
         return True
 
 def docker_remove_settings(docker_client, use_local_socket, docker_settings):
+    logger = logging.getLogger(__name__)
     container_name = docker_settings['name']
-    print('Finding container: ', container_name)
+    logger.info('Finding container: {container_name}'.format(**locals()))
     if len(docker_client.containers.list(all=True, sparse=True, filters= {'name':container_name})) > 0:
         container = docker_client.containers.get(container_name)
         if container:
-            print('Found container')
-            event = threading.Event()
+            logger.info('Found container')
+            removed = threading.Event()
             wait_ready = threading.Event()
-            t = threading.Thread(target=wait_container_status, args = (create_client(use_local_socket), docker_settings,'removed',wait_ready,event,10))
+            t = threading.Thread(target=wait_container_status, args = (create_client(use_local_socket), docker_settings,'removed',wait_ready,removed,10))
             t.daemon = True
             t.start()
             wait_ready.wait()
             container.remove(force=True)
-            print('Waiting...')
-            event.wait()
-            print('Removed container')
+            logger.info('Waiting...')
+            removed.wait()
+            logger.info('Removed container')
     return False
 
 def container_get_status(docker_client, docker_settings):
@@ -95,13 +92,12 @@ def docker_logs_settings(docker_client, docker_settings):
     except NotFound:
         return 'Container not found'
 
-def wait_container_status(docker_client, docker_settings, condition, wait_ready, status_achieved, timeout=10, max_tries=1):
+def wait_container_status(docker_client, docker_settings, condition, wait_ready, status_achieved, timeout=10, max_tries=1, tries=0):
     
     if not status_achieved:
-        raise ValueError('status_achieved must not be null')
+        raise ValueError('status_achieved must contain a value')
 
     container_name = docker_settings['name']
-    tries = 0
     try:
         container = find_container(docker_client, container_name)
 
@@ -112,7 +108,7 @@ def wait_container_status(docker_client, docker_settings, condition, wait_ready,
     except ConnectionError as ex:
         if tries < max_tries:
             tries += 1
-            wait_container_status(docker_client, docker_settings, condition, wait_ready, status_achieved, timeout, tries)
+            wait_container_status(docker_client, docker_settings, condition, wait_ready, status_achieved, timeout, tries=tries)
         else:
             raise ex
     except NotFound as ex:
@@ -122,7 +118,8 @@ def wait_container_status(docker_client, docker_settings, condition, wait_ready,
             raise ex
 
 def find_container(docker_client, container_name):
-    print('Finding container: ', container_name)
+    logger=logging.getLogger(__name__)
+    logger.info('Finding container: {container_name}'.format(**locals()))
     if len(docker_client.containers.list(all=True, sparse=True, filters= {'name':container_name})) > 0:
         return docker_client.containers.get(container_name)
     else:
@@ -135,19 +132,19 @@ class DockerConfig():
         self.use_local_socket = use_local_socket
 
     def base_url(self):
-        if not self.use_local_socket and 'DOCKER_BASE_URL' in os.environ and not os.environ['DOCKER_BASE_URL'] == '' :
+        if not self.use_local_socket and 'DOCKER_BASE_URL' in os.environ and os.environ['DOCKER_BASE_URL']:
             return os.environ['DOCKER_BASE_URL']
         else:
             return 'unix:///var/run/docker.sock'
 
     def version(self):
-        if  'DOCKER_VERSION' in os.environ and not os.environ['DOCKER_VERSION'] == '':
+        if  'DOCKER_VERSION' in os.environ and  os.environ['DOCKER_VERSION']:
             return os.environ['DOCKER_VERSION']
         else:
             return 'auto'
 
     def timeout(self):
-        if 'DOCKER_TIMEOUT' in os.environ and not os.environ['DOCKER_TIMEOUT'] == '':
+        if 'DOCKER_TIMEOUT' in os.environ and os.environ['DOCKER_TIMEOUT']:
             return int(os.environ['DOCKER_TIMEOUT'])
         else:
             return 120
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name='pvs-dockerlib',  
-    version='0.0.3',
+    version='0.0.4',
     author="Peel Valley Softwware",
     author_email="info@peelvalley.com.au",
     description="Custom docker client wrapper",
